306f3270a33c69da2be2b30f737c14bdd7bf1cc1
Kelvin Modesto <km.modesto@gmail.com>
2020-09-09 23:03:22 -0300

    :rotating_light: (eslint) Add eslint plugins and update tsconfig file

---
+++ .eslintrc.js
@@ -0,0 +1,37 @@
+module.exports = {
+  env: {
+    es6: true,
+    node: true,
+  },
+  extends: [
+    'airbnb-base',
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
+    'plugin:import/typescript',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 11,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint', 'prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+    'import/extensions': ['error', 'ignorePackages', { ts: 'never' }],
+    'import/prefer-default-export': 'off',
+    'import/no-extraneous-dependencies': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    'consistent-return': 'off',
+    'no-console': ['error', { allow: ['error'] }],
+    'no-bitwise': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+  },
+  settings: {
+    'import/resolver': {
+      typescript: {
+        project: './src',
+      },
+    },
+  },
+};
