2ce09639f83f4da3ca3557bb91bc3ba39aecb0d8
Kelvin Modesto <km.modesto@gmail.com>
2020-09-09 23:52:58 -0300

    :construction: (app.ts) create main app file

---
+++ src/app.ts
@@ -0,0 +1,10 @@
+import cookieParser from 'cookie-parser';
+import express from 'express';
+import morgan from 'morgan';
+
+const app = express();
+
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
